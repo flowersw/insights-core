7b9dc5fb3a984c6adb4d43e00e1ec7b1d70c421d
Alec Cohan <alcohan@redhat.com>
2021-02-01 15:52:49 -0500

    RHCLOUD-11737: Implement playbook signature validation logic in inisghts-core
    Signed-off-by: Alec Cohan <alcohan@redhat.com>

--- insights/client/apps/ansible/playbook_verifier/__main__.py
+++ insights/client/apps/ansible/playbook_verifier/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import oyaml as yaml
 
 from insights.client.apps.ansible.playbook_verifier import verify
 
@@ -14,12 +15,13 @@ def read_playbook():
     return unverified_playbook
 
 
-unverified_playbook = read_playbook()
+playbook = read_playbook()
+playbook_yaml = yaml.load(playbook)
 
 try:
-    verified_playbook = verify(unverified_playbook)
+    verified_playbook = verify(playbook_yaml, checkVersion=False, skipVerify=True)
 except Exception as e:
     sys.stderr.write(e.message)
     sys.exit(1)
 
-print(verified_playbook)
+print(playbook)
